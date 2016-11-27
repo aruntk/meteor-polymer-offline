1393c8a2ecd3404d44e9ac1875eb7a13d5340cdf
aruntk <aruntknambiar@gmail.com>
2016-11-28 00:44:59 +0530

    ui caching using sw

--- imports/startup/client/index.js
+++ imports/startup/client/index.js
@@ -1,3 +1,4 @@
-console.log('%cEnjoy the %cM %cW %cC %cgoodness','color:#d61a7f;', 'color:red;font-size:50px;font-weight:bold;font-family:sans-serif;','color:green;font-size:50px;font-weight:bold;font-family:sans-serif;','color:blue;font-size:50px;font-weight:bold;font-family:sans-serif;','color:#d61a7f;');
-
 import '../../ui';
+
+console.log('%cMeteor %cM %cW %cC %cPolymer', 'color:#d61a7f;', 'color:red;font-size:50px;font-weight:bold;font-family:sans-serif;', 'color:green;font-size:50px;font-weight:bold;font-family:sans-serif;', 'color:blue;font-size:50px;font-weight:bold;font-family:sans-serif;', 'color:#d61a7f;');
+
